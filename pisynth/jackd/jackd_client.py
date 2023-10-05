8a9dc094914bc4a5cc4c0a7b510430dc479b74b3
Lucas Egidio <lucasegidio1@gmail.com>
2023-10-05 22:50:23 +0200

    ADD: output/input connect to qsynth

--- pisynth/jackd/jackd_client.py
+++ pisynth/jackd/jackd_client.py
@@ -1,17 +1,108 @@
 from dataclasses import dataclass
+import enum
+from enum import _EnumDict
+import re
 
-from typing import Dict, List
+from typing import Any, Dict, Iterable, List, Union
 import jack
+
+SAME_NAME_CHANNELS = {
+    "FR": ["FR", "RIGHT", "FRONTRIGHT"],
+    "FL": ["FL", "LEFT", "FRONTLEFT"],
+    "BR": ["BR", "REARRIGHT", "BACKRIGHT", "RR"],
+    "BL": ["BL", "REARLEFT", "BACKLEFT", "RL"],
+    "C": ["C", "CENTER", "CENTRE"],
+}
+
+class AudioChannel(str, enum.Enum):
+    FR = "FR"
+    FL = "FL"
+    BR = "BR"
+    BL = "BL"
+    C = "C"
+    
+    # def __new__(cls, value):
+    #     for key, values in SAME_NAME_CHANNELS.items():
+    #         if value in values:
+    #             obj = str.__new__(cls)
+    #             obj._value_ = key
+    #             for v in values:
+    #                 cls._value2member_map_[v] = obj 
+                    
+    #             obj._all_values = values
+    #     return super().__new__(cls, value)
+
+    def __eq__(self, __value) -> bool:
+        if isinstance(__value, str):
+            return self._value_ == __value.upper()
+        if isinstance(__value, AudioChannel):
+            return self._value_ == __value._value_
+        return False
+    
+    @classmethod
+    def _missing_(cls, value):
+        value = value.upper()
+        for member in cls:
+            if value in SAME_NAME_CHANNELS[member._value_]:
+                return member
+        return None
+
+    def __hash__(self) -> int:
+        return self._value_.__hash__()
 
 
 _CLIENT = jack.Client("pysynth")
 
+def rev(in_str: str) -> str:
+    return in_str[::-1]
+
+def _get_channel(in_str: Union[str, jack.Port]) -> AudioChannel:
+    if isinstance(in_str, jack.Port):
+        in_str = in_str.name
+    return AudioChannel(rev(re.split(':|_', rev(in_str), maxsplit=1)[0]))
+
+def _get_name_without_channel(in_str: str) -> str:
+    return rev(':'.join(re.split(':|_', rev(in_str), maxsplit=1)[1:]))
 
 @dataclass
 class QSynth:
+    
     midi_in: Dict[str, jack.MidiPort]
     audio_out: Dict[str, jack.Port]
+    
+    def connect_midi_input(self, port: Union[jack.MidiPort, str] ):
+        client = get_client()
+        for in_port in self.midi_in:
+            for c in client.get_all_connections(in_port):
+                    client.disconnect(c, in_port)
+            client.connect(port, in_port)
 
+    
+    def connect_audio_output(self, port: Union[jack.Port, str] ):
+        client = get_client()
+        port_name = port if isinstance(port, str) else port.name
+        outputs = [v for k, v in get_default_outputs().items() if port_name in k]
+        if len(outputs) == 0:
+            raise ValueError("Port not found")
+        
+        outputs = outputs[0]  # take first output
+        if len(outputs)==1: # mono
+            for out_port in self.audio_out:
+                for c in client.get_all_connections(out_port):
+                    client.disconnect(out_port, c)
+                client.connect(out_port, port)
+        if len(outputs)>1: # multiple
+            channels_to_name = _build_channels_to_name_map(outputs)
+            for out_port in self.audio_out:
+                for c in client.get_all_connections(out_port):
+                    client.disconnect(out_port, c)
+                ch = _get_channel(out_port)
+                client.connect(out_port, channels_to_name[ch])
+            
+
+
+    def check_connections(self):
+        pass
 
 def get_client() -> jack.Client:
     _CLIENT.activate()
@@ -52,6 +143,13 @@ def get_qsynth():
 
     return QSynth(midi_in=midi_in, audio_out=audio_out)
 
-def get_default_output() -> Dict[str, jack.Port]:
+def get_default_outputs(stereo=True) -> Dict[str, jack.Port]:
     outputs = get_audio_ports(input=True)
-    return outputs
+    if not stereo:    
+        return outputs
+    keys = set(_get_name_without_channel(n) for n in outputs)
+    return {k: [v for _, v in outputs.items() if k in _] for k in keys}
+    
+def _build_channels_to_name_map(outputs: Dict[str, jack.Port]) -> Dict[AudioChannel, str]:
+    return {_get_channel(key): key for key in outputs}
+    
--- pisynth/server/server.py
+++ pisynth/server/server.py
@@ -3,7 +3,7 @@ from logging import getLogger
 
 from flask import Flask, request, render_template
 
-from pisynth.jackd.jackd_client import get_client, get_midi_ports, get_qsynth, get_default_output
+from pisynth.jackd.jackd_client import get_client, get_midi_ports, get_qsynth, get_default_outputs
 
 logger = getLogger(__name__)
 logger.setLevel("INFO")
@@ -18,26 +18,36 @@ parameters = {
     "slider_value": 50,
     "checkbox_status": False,
     "midi_device": None,
+    "output_device": None,
 }
 
-options = ["option1", "option2", "option3"]
 
 @app.route("/", methods=["GET", "POST"])
 def index():
 
-    midi_ports = get_midi_ports()
+    midi_ports = get_midi_ports(output=True)
+    outputs = get_default_outputs()
+    qsynth.check_connections()
     if request.method == "POST":
         # Update parameters based on form data
         logger.info(request.form)
         parameters["slider_value"] = int(request.form["slider"])
         parameters["checkbox_status"] = "checkbox" in request.form
-        parameters["midi_device"] = request.form["midi_device"]
+        if parameters["midi_device"] != request.form["midi_device"]:
+            parameters["midi_device"] = request.form["midi_device"]
+            qsynth.connect_midi_input(request.form["midi_device"])
+        
+        if parameters["output_device"] != request.form["output_device"]:
+            parameters["output_device"] = request.form["output_device"]
+            qsynth.connect_audio_output(request.form["output_device"])
+        
 
-    return render_template(index_path, parameters=parameters, options=midi_ports.keys())
+
+    return render_template(index_path, parameters=parameters, midi_devices=midi_ports.keys(), output_devices=outputs.keys())
 
 def init():
     global qsynth, output_device
-    output_device = get_default_output()
+    output_device = get_default_outputs()
     qsynth = get_qsynth()
     app.run(debug=True)
     
